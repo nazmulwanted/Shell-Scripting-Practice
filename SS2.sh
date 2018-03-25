10feedd7072f29258d45fe75ed85dd6d3cea0661
Nazmul <nazmulwanted@gmail.com>
2018-03-26 00:58:20 +0600

    Changed name of the files

---
+++ SS2.sh
@@ -0,0 +1,9 @@
+#SS2
+#Usage: SS2
+#An interactive shell script
+
+echo What is your name?
+read name
+echo Hello $name, Happy programming!
+echo "This is just a test program!"
+echo "This is another line"
